8d4c5841e041683bef2d2fea17adbece46085cf2
alessandro <tavazale@gmail.com>
2022-06-03 00:43:11 +0200

    corrections

--- website/js/d3stats.js
+++ website/js/d3stats.js
@@ -1,5 +1,6 @@
 var i = 0;
 
+
 var mypositionoil = [131.98378134855085,2.5524576027594135,
   131.98378134855085, 5.104915205518827,
   131.98378134855085,7.657372808278241,
@@ -150,60 +151,54 @@ var mypositionoil = [131.98378134855085,2.5524576027594135,
 
 function prevshowOrHideDiv() {
   i=i-1
-  if (i<0){
-    alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");
-}
-  if (i>146){
-    alert("We don't show data for the selected date!  Data range: 01.01.2022-26.05.2022");
-  }
-  else{
+  if (146>i>-1){
     oilshowOrHideDiv()
     gasshowOrHideDiv()
-  mapprevshowOrHideDiv(i+1)}
+mapprevshowOrHideDiv(i+1)
+    /*alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");*/
+}
+  else{
+    i = i+1
+  }
 }
 
 function nextshowOrHideDiv() {
   i=i+1
-  if (i<0){
-    alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");
-}
-  if (i>145){
-    alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");
-  }
-  else{
+  if (146>i>-1){
     oilshowOrHideDiv()
     gasshowOrHideDiv()
-    mapnextshowOrHideDiv(i-1)}
-
+mapprevshowOrHideDiv(i-1)
+    /*alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");*/
 }
+  else{
+    i = i-1
+  }}
 
 function superprevshowOrHideDiv() {
   i=i-7
-  if (i<0){
-    alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");
-}
-  if (i>145){
-    alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");
-  }
-  else{
+  if (146>i>-1){
     oilshowOrHideDiv()
     gasshowOrHideDiv()
-mapprevshowOrHideDiv(i+7)}
+mapprevshowOrHideDiv(i+7)
+    /*alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");*/
+}
+  else{
+    i = i+7
+  }
 
 }
 
 function supernextshowOrHideDiv() {
   i=i+7
-  if (i<0){
-    alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");
-}
-  if (i>145){
-    alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");
-  }
-  else{
+  if (146>i>-1){
     oilshowOrHideDiv()
     gasshowOrHideDiv()
-mapnextshowOrHideDiv(i-7)}
+mapprevshowOrHideDiv(i-7)
+    /*alert("We don't show data for the selected date! Data range: 01.01.2022-26.05.2022");*/
+}
+  else{
+    i = i-7
+  }
 }
 
 //d3.select("body").append("p").text("New paragraph!");
@@ -679,7 +674,6 @@ var mypositiongas = [0 ,127.238 ,
 
 //Read the data
 d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-independentua/main/data/economic/gas_2021.csv",
-
   // When reading the csv, I must format variables:
   function(d){
     return { date : d3.timeParse("%Y-%m-%d")(d.Date), value : d.Price}
@@ -758,13 +752,167 @@ d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-pro
 
 })
 
+var mydays = [0,
+2,
+3,
+4,
+5,
+6,
+7,
+8,
+9,
+10,
+11,
+12,
+13,
+14,
+15,
+16,
+17,
+18,
+19,
+20,
+21,
+22,
+23,
+24,
+25,
+26,
+27,
+28,
+29,
+30,
+31,
+32,
+33,
+34,
+35,
+36,
+37,
+38,
+39,
+40,
+41,
+42,
+43,
+44,
+45,
+46,
+47,
+48,
+49,
+50,
+51,
+52,
+53,
+54,
+55,
+56,
+57,
+58,
+59,
+60,
+61,
+62,
+63,
+64,
+65,
+66,
+67,
+68,
+69,
+70,
+71,
+72,
+73,
+74,
+75,
+76,
+77,
+78,
+79,
+80,
+81,
+82,
+83,
+84,
+85,
+86,
+87,
+88,
+89,
+90,
+91,
+92,
+93,
+94,
+95,
+96,
+97,
+98,
+99,
+100,
+101,
+102,
+103,
+104,
+105,
+106,
+107,
+108,
+109,
+110,
+111,
+112,
+113,
+114,
+115,
+116,
+117,
+118,
+119,
+120,
+121,
+122,
+123,
+124,
+125,
+126,
+127,
+128,
+129,
+130,
+131,
+132,
+133,
+134,
+135,
+136,
+137,
+138,
+139,
+140,
+141,
+142,
+143,
+144,
+145,
+146
+]
+
 
 
 visualizemydata(mydayarray[0],array[0])
 
 
  function mapprevshowOrHideDiv(j) {
-   visualizemydata(mydayarray[i],array[i])
+   if (mydays[i]!==0){
+     visualizemydata(mydayarray[i],array[i])
+     mydays[i] =0
+   console.log(mydays)}
+
+
+   var v = document.getElementById(array[i]);
     if (v.style.display === "none") {
        v.style.display = "block";
     } else {
@@ -778,7 +926,11 @@ visualizemydata(mydayarray[0],array[0])
     }
  }
  function mapnextshowOrHideDiv(j) {
-   visualizemydata(mydayarray[i],array[i])
+   if (mydays[i]!==0){
+     visualizemydata(mydayarray[i],array[i])
+     mydays[i] =0
+    console.log(mydays)}
+
     var v = document.getElementById(array[i]);
     if (v.style.display === "none") {
        v.style.display = "block";
